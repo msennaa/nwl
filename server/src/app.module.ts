255ee5eea4192b4370e5e2e0b6973205a484c6df
MSenaaa <senamatheusjob@gmail.com>
2022-09-15 22:16:49 -0300

    conexão com banco de dados

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdsModule } from './ads/ads.module';
 
 @Module({
-  imports: [AdsModule],
+  imports: [
+    AdsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '12345678',
+      database: 'nextjs',
+      entities: ['dist/**/*.model.js'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
